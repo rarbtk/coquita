6c19003ccc6e092c64675e119967bed8852be37e
Luis <luisbattle@hotmail.com.ar>
2020-09-25 00:58:30 -0300

    se inicializa proyecto node y se agrega estructura de archivos

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+/*
+Rutas aqui.........
+*/
+app.listen(3002, () => {
+  console.log("##############################");
+  console.log("Proyecto Coquita is running :)");
+  console.log("##############################");
+});
