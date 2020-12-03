291c7baa0ee72162fed74a25d07061e2a67c0111
Luis <luisbattle@hotmail.com.ar>
2020-12-03 00:24:39 -0300

    se agrega middleware isAuthenticated y agrega en que para ingresar a carrito se debe estar logueado sino redirecciona al login

--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const userControllers = require("../controllers/userControllers");
+const isAuthenticatedMiddleware = require("../middleware/isAuthenticatedMiddleware");
 
 /* CART. */
-router.get("/cart", function (req, res, next) {
+router.get("/cart", isAuthenticatedMiddleware, function (req, res, next) {
   res.render("cart/cart");
 });
 module.exports = router;
