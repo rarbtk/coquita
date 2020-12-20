343725b210490b6d68b93badda5bd440f73f0e90
Luis <luisbattle@hotmail.com.ar>
2020-12-20 17:35:40 -0300

    se agrega obtencion de promociones de la DB en la home

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const db = require("../database/models");
 const products = require("../data/products.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  console.log("locals", res.locals);
-  res.render("index", { products, currentUser: req.session.user });
+  db.Product.findAll({
+    where: {
+      category_id: 2, //Promociones - bestSellers
+    },
+  }).then((bestSellers) => {
+    res.render("index", {
+      bestSellers,
+      currentUser: req.session.user,
+    });
+  });
+  //console.log("locals", res.locals);
 });
 
 router.get("/contact", function (req, res, next) {
