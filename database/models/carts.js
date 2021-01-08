508f88bf738b5bb75634182e1d46f3fa28221abb
unknown <rarbtk10@gmail.com>
2021-01-07 21:16:56 -0300

    se agregaron el modelo de la base de datos de cart_items y carts

---
+++ database/models/carts.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, dataTypes){
+    let alias = "carts";
+
+    let cols = {
+        id :{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        
+        
+    };
+
+    let config = {
+        tableName = "carts",
+        timestamps = false,
+    }
+
+    let carts = sequelize.define(alias,cols,config);
+
+    carts.associate = function(models){
+        carts.belongsTo(models.User,{
+            as :"User",  
+            foreignKey: "user_id"
+        })
+    }
+
+    return carts
+}
