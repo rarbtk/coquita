28aaa7cdbb0ca139ec4e3e3d0618f9fce0a35858
Luis <luisbattle@hotmail.com.ar>
2021-01-08 00:44:22 -0300

    se corrige tabla de modelos cartItems y carts

---
+++ database/models/cartItems.js
@@ -0,0 +1,42 @@
+module.exports = function (sequelize, dataTypes) {
+  let alias = "CartItems";
+
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type: dataTypes.INTEGER,
+    },
+
+    quantity: {
+      type: dataTypes.INTEGER,
+    },
+
+    cart_id: {
+      type: dataTypes.INTEGER,
+    },
+  };
+  let config = {
+    tableName: "cart_items",
+    timestamps: false,
+  };
+
+  let CartItems = sequelize.define(alias, cols, config);
+
+  CartItems.associate = function (models) {
+    CartItems.belongsTo(models.Product, {
+      as: "Product",
+      foreignKey: "product_id",
+    });
+    CartItems.belongsTo(models.Carts, {
+      as: "Carts",
+      foreignKey: "cart_id",
+    });
+  };
+
+  return CartItems;
+};
--- database/models/carts.js
+++ database/models/carts.js
@@ -1,29 +1,27 @@
-module.exports = function (sequelize, dataTypes){
-    let alias = "carts";
+module.exports = function (sequelize, dataTypes) {
+  let alias = "Carts";
 
-    let cols = {
-        id :{
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-        
-        
-    };
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  };
 
-    let config = {
-        tableName = "carts",
-        timestamps = false,
-    }
+  let config = {
+    tableName: "carts",
+    timestamps: false,
+  };
 
-    let carts = sequelize.define(alias,cols,config);
+  let carts = sequelize.define(alias, cols, config);
 
-    carts.associate = function(models){
-        carts.belongsTo(models.User,{
-            as :"User",  
-            foreignKey: "user_id"
-        })
-    }
+  carts.associate = function (models) {
+    carts.belongsTo(models.User, {
+      as: "User",
+      foreignKey: "user_id",
+    });
+  };
 
-    return carts
-}
+  return carts;
+};
