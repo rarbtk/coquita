f3537e59f5e6fc1282c2e53898bb8e145e9057ef
Luis <luisbattle@hotmail.com.ar>
2021-01-12 20:22:52 -0300

    se agrega dotenv al database/config.json

--- database/config/config.js
+++ database/config/config.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "coquitadb",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: "127.0.0.1",
     dialect: "mysql",
   },
