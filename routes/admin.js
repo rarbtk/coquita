6aa5fd9993c1c3e175c67acade03de99a928f684
Daniel <dany_9688@hotmail.com>
2021-02-17 12:08:10 -0300

    se arregló ruteo de profile y se agregó botón de nuevo producto

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var adminMiddleware = require("../middleware/adminMiddleware")
 
 const adminController = require("../controllers/adminController.js")
-const userControllers = require("../controllers/userControllers.js")
+const userController = require("../controllers/userControllers.js")
 /* GET home page. */
 router.get("/", adminController.products);
-router.get("/profile/:id", adminController.profile);
+router.get("/profile/:id", userController.profile);
 router.post("/delete/:id", adminController.deleteUser);
 
 module.exports = router;
